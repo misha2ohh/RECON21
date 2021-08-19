9c33059cad957cca8796dce8bd977b8b5f554382
misha2ohh <63752901+misha2ohh@users.noreply.github.com>
2021-08-19 08:50:06 -0500

    Add files via upload
    This is a simplified explanation of what the different parts of an Arduino sketch are for.

---
+++ Puzzle_Pseudo_Code_Basic_v1.ino
@@ -0,0 +1,13 @@
+//grocery shopping happens here
+
+void setup() {
+  // put your setup code here, to run once:
+  Food prep happens here
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  This is where we start cooking!
+}
+
+//recipes for our side dishes here!
