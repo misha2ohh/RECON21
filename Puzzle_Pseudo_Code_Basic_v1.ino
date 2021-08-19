9f710f66b1037d69c808fc68d37defb96fe12aa1
misha2ohh <63752901+misha2ohh@users.noreply.github.com>
2021-08-19 09:32:40 -0500

    Update Puzzle_Pseudo_Code_Basic_v1.ino

--- Puzzle_Pseudo_Code_Basic_v1.ino
+++ Puzzle_Pseudo_Code_Basic_v1.ino
@@ -1,4 +1,22 @@
-//grocery shopping happens here
+/*
+ * Puzzle Pseudo Code Basic v1
+ * 
+ * This is code for the All-in-One Cabinet Mag Lock puzzle.
+ * 
+ * The tutorial for this puzzle was originally presented at RECON '21 during the "How to Build and Refine Your First Escape Room Tech Puzzle"
+ * 
+ * The idea behind this particular file is to explain the different parts of an Arduino sketch using the metaphor of making dinner for a friend.
+ * 
+ * Created 2021
+ * by Karmisha Jawell https://github.com/misha2ohh/
+ * 
+ * This example code is in the public domain.
+ * 
+ * https://github.com/misha2ohh/RECON21
+ * 
+ */
+ 
+ //grocery shopping happens here
 
 void setup() {
   // put your setup code here, to run once:
